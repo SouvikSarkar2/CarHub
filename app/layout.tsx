8b16b6a4e06afe0e718f868597fb7409055e056c
Souvik Sarkar <12345souvik12345@gmail.com>
2023-12-27 11:51:21 +0530

    Finalized

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,17 @@
-import { Footer, Navbar } from "@/components";
 import "./globals.css";
+
+import { Footer, NavBar } from "@components";
 
 export const metadata = {
   title: "Car Hub",
-  description: "Discover the best cars in the World ",
+  description: "Discover world's best car showcase application",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className="relative">
-        <Navbar />
+    <html lang='en'>
+      <body className='relative'>
+        <NavBar />
         {children}
         <Footer />
       </body>
